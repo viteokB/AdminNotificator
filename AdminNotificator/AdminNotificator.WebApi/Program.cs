c2c145c509898e86b6118c0286aaaa1ccf7822be
Braidenbikher Victor <viktor.brai@yandex.ru>
2025-04-14 12:38:29 +0500

    Added swagergen with versioning in headers

--- AdminNotificator/AdminNotificator.WebApi/Program.cs
+++ AdminNotificator/AdminNotificator.WebApi/Program.cs
@@ -1,6 +1,42 @@
+using System.Reflection;
+using Asp.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    //Времено отключил комментарии для контроллеров НАСТРОИТЬ ФАЙЛ ПОЗЖЕ!!!!
+    // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    // swagger.IncludeXmlComments(xmlPath);
+    
+    swagger.SupportNonNullableReferenceTypes();
+});
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new HeaderApiVersionReader("api-version"));
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+
+    });
+}
 
 app.UseHttpsRedirection();
 app.Run();
