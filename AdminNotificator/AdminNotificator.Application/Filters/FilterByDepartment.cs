6c8a98903e1095a7720e7870fb1d72105a96f8b5
Савелий <vildanov.s@bk.ru>
2025-04-14 16:12:48 +0500

    Filter

---
+++ AdminNotificator/AdminNotificator.Application/Filters/FilterByDepartment.cs
@@ -0,0 +1,30 @@
+﻿using AdminNotificator.Core.Domain;
+using AdminNotificator.Core.Repositories;
+
+namespace AdminNotificator.Application.Services.Filters;
+
+public class FilterByDepartment : IUserFilter
+{
+    private readonly IRepository<UserProfile> userProfileRepository;
+
+    FilterByDepartment(IRepository<UserProfile> userProfileRepository)
+    {
+        this.userProfileRepository = userProfileRepository;
+    }
+
+
+    public Task<IEnumerable<UserProfile>> GetUserByFilter(EmailType emailType)
+    {
+        var targetDepartments= emailType.IntersectDepartmentIds;
+        if (targetDepartments == null)
+        {
+            return Task.FromResult<IEnumerable<UserProfile>>(null);
+        }
+        var resultUsers = userProfileRepository.GetAll()
+            .Where(user => user.DepartmentItems != null
+                           && user.DepartmentItems.Any(department =>
+                               targetDepartments.Contains(department.Id)))
+            .ToList();
+        return Task.FromResult(resultUsers.AsEnumerable());
+    }
+}
---
+++ AdminNotificator/AdminNotificator.Application/Filters/FilterByPosts.cs
@@ -0,0 +1,29 @@
+﻿using AdminNotificator.Core.Domain;
+using AdminNotificator.Core.Repositories;
+
+namespace AdminNotificator.Application.Services.Filters;
+
+public class FilterByPosts : IUserFilter
+{
+    private readonly IRepository<UserProfile> userProfileRepository;
+
+    public FilterByPosts(IRepository<UserProfile> userProfileRepository)
+    {
+        this.userProfileRepository = userProfileRepository;
+    }
+
+    public Task<IEnumerable<UserProfile>> GetUserByFilter(EmailType emailType)
+    {
+        var targetPosts = emailType.IntersectUserPosts;
+        if (targetPosts == null)
+        {
+            return Task.FromResult<IEnumerable<UserProfile>>(null);
+        }
+        var resultUsers = userProfileRepository.GetAll()
+            .Where(user => user.UserPositions != null
+                           && user.UserPositions.Any(post =>
+                               targetPosts.Contains(post.Post)))
+            .ToList();
+        return Task.FromResult(resultUsers.AsEnumerable());
+    }
+}
